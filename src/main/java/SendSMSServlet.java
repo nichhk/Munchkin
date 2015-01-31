c3876a275675953ffce5cc7c1896eec20f31dd93
Vegetable26 <jhwang261@gmail.com>
2015-01-31 16:01:57 -0600

    Added TripServlet

--- src/main/java/SendSMSServlet.java
+++ src/main/java/SendSMSServlet.java
@@ -1,9 +1,17 @@
-package main.java;
+
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by compsci on 1/31/15.
  */
 public class SendSMSServlet extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {}
+
+
+
+
 }
--- src/main/java/TransactionServlet.java
+++ src/main/java/TransactionServlet.java
@@ -1,10 +1,14 @@
-package main.java;
+
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by compsci on 1/31/15.
  */
 public class TransactionServlet extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {}
 
 }
