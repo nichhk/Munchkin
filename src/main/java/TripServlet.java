b6b34a4afda5d0e88ee8f4d23e38982b7cb1459e
Vegetable26 <jhwang261@gmail.com>
2015-01-31 16:01:43 -0600

    Added beginning of TripServlet

--- src/main/java/TripServlet.java
+++ src/main/java/TripServlet.java
@@ -1,10 +1,48 @@
-package main.java;
+
+
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by compsci on 1/31/15.
  */
 public class TripServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        double currentTime = System.currentTimeMillis();
+        UserService userService = UserServiceFactory.getUserService(); // Finds the user's email from OAuth
+        User user = userService.getCurrentUser();
+        String email = user.getEmail();
+        Entity trip = new Entity("trip", Double.toString(currentTime)); // Trips are Id'd by their timeStamp
+
+        trip.setProperty("user", req.getParameter(""));
+        trip.setProperty("eta", req.getParameter("")); // Estimated time of arrival
+        trip.setProperty("etd", req.getParameter("")); // Estimated time of delivery
+        trip.setProperty("restaurantTime", req.getParameter(""));
+        trip.setProperty("restaurant", req.getParameter(""));
+        trip.setProperty("flat",findFee(req));
+        trip.setProperty("percentage",req.getParameter(""));
+        trip.setProperty("maxOrder",req.getParameter(""));
+        trip.setProperty("acceptUntil",req.getParameter());
+
+
+    }
+    private double findFee(HttpServletRequest req){
+        if(req.getParameter("flat")){
+
+        }
+        else if(req.getParameter("perc")){
+
+        }
+
+    }
+
 }
 
