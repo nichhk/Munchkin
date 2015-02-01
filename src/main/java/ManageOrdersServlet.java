9bdb14e3d16c7babd347524937c81156b12bc2ac
Vegetable26 <jhwang261@gmail.com>
2015-02-01 08:28:10 -0600

    fixed order

--- src/main/java/ManageOrdersServlet.java
+++ src/main/java/ManageOrdersServlet.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 import java.util.List;
 import com.google.appengine.api.datastore.*;
 import com.google.appengine.api.users.*;
+import com.google.gson.Gson;
+
 /**
  * Created by compsci on 2/1/15.
  */
@@ -20,6 +22,8 @@ public class ManageOrdersServlet extends HttpServlet {
     private List<Trip> myTrips = new ArrayList<Trip>();
     private static DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     public void doGet(HttpServletRequest req, HttpServletResponse resp){
+        myOrders = new ArrayList<Order>();
+        myTrips = new ArrayList<Trip>();
         UserService userService = UserServiceFactory.getUserService(); // Finds the user's email from OAuth
         com.google.appengine.api.users.User user = userService.getCurrentUser();
         String email = user.getEmail();
@@ -32,7 +36,7 @@ public class ManageOrdersServlet extends HttpServlet {
             System.out.println("cooking order");
             myOrders.add(new Order(order));
             try {
-                Query q1 = new Query("order");
+                Query q1 = new Query("trip");
                 Query.Filter myTrip = new Query.FilterPredicate("time",
                         Query.FilterOperator.EQUAL, order.getProperty("tripId"));
                 PreparedQuery pq1 = datastore.prepare(q1.setFilter(onlyMine));
@@ -44,6 +48,11 @@ public class ManageOrdersServlet extends HttpServlet {
             }
         }
         System.out.println("IN MANAGE Order");
+        req.setAttribute("responseJson", new Gson().toJson(myOrders));
+        System.out.println(new Gson().toJson(myOrders));
+        try {
+            req.getRequestDispatcher("ManageOrder.jsp").forward(req, resp);
+        } catch (Exception e) {e.printStackTrace();}
 
 
 
