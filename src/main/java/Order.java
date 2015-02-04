23d4429c02b61348f536d5809df079002bf35e85
Vegetable26 <jhwang261@gmail.com>
2015-02-03 23:25:14 -0600

    Made order objectA

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,25 +1,39 @@
+import com.google.appengine.api.datastore.EmbeddedEntity;
 import com.google.appengine.api.datastore.Entity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by nich on 1/31/15.
  */
 public class Order {
-    String email;
-    String tripId;
+    List<Item> items = new ArrayList<Item>();
     public Order(Entity order){
-        this.email = (String)order.getProperty("email");
-        this.tripId = (String)order.getProperty("trip");
+        List<EmbeddedEntity> itemsEntity= (ArrayList<EmbeddedEntity>)(order.getProperty("items"));
+        for(EmbeddedEntity item: itemsEntity){
+            items.add(new Item(item,false));
+        }
     }
+
     public class Item{
         public String foodItem;
         public String priceMax;
         public String comments;
-        public Item alt;
+        public Item alt = null;
         private boolean isAlt;
-        public Item(Entity item){
-            this.foodItem = (String)item.getProperty("foodItem");
-            this.priceMax = (String)item.getProperty("priceMax");
-            this.comments = (String)item.getProperty("comments");
+
+        public Item(EmbeddedEntity item, boolean isAlt) {
+            this.isAlt = isAlt;
+            this.foodItem = (String) item.getProperty("foodItem");
+            this.priceMax = (String) item.getProperty("priceMax");
+            this.comments = (String) item.getProperty("comments");
+            if (!isAlt) {
+                if (item.getProperty("alt") != null) {
+                    alt = new Item((EmbeddedEntity) item.getProperty("alt"), true);
+                }
+            }
         }
+
     }
 }
